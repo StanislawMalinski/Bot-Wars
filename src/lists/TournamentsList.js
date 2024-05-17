56c7b3dce6c095d23cd695f8ab0758e1bf08c9ae
Mikołaj <mikibur@wp.pl>
2024-05-17 14:27:43 +0200

    Tournaments list

--- src/lists/TournamentsList.js
+++ src/lists/TournamentsList.js
@@ -9,21 +9,20 @@ import React, { useState, useEffect, useMemo } from "react";
 
 function TournamentsList({ tournaments, isAuthenticated }) {
     const navigate = useNavigate();
-    const currentDate = new Date();
-    const [upcomingTournaments, setUpcomingTournaments] = useState([]);
-    const [pastTournaments, setPastTournaments] = useState([]);
+    const currentDate = useMemo(() => new Date(), []);
+    const [filteredTournaments, setFilteredTournaments] = useState([]);
+    const [allTournaments, setAllTournaments] = useState([]);
+    const [tournamentTitle, setTournamentTitle] = useState('');
+    const [minPlayOutDate, setMinPlayOutDate] = useState('');
+    const [maxPlayOutDate, setMaxPlayOutDate] = useState('');
+    const [creator, setCreator] = useState('');
+    const [userParticipation, setUserParticipation] = useState('');
     const [message, setMessage] = useState('');
     
     useEffect(() => {
-        TournamentService.getFilteredTournaments(0, 10, {minPlayOutDate: currentDate.toISOString()})
+        TournamentService.getFilteredTournaments(0, 10, {minPlayOutDate: "1028-05-17T09:14:04.525Z", maxPlayOutDate: "3028-05-17T09:14:04.525Z"})
             .then((response) => {
-                setUpcomingTournaments(response.data.data.page);
-            }).catch((error) => {
-                setMessage('Error loading tournaments');
-            });
-        TournamentService.getFilteredTournaments(0, 10, {maxPlayOutDate: currentDate.toISOString()})
-            .then((response) => {
-                setPastTournaments(response.data.data.page);
+                setAllTournaments(response.data.data.page);
             }).catch((error) => {
                 setMessage('Error loading tournaments');
             });
@@ -38,53 +37,146 @@ function TournamentsList({ tournaments, isAuthenticated }) {
             <div className="tournament-detail">{tournament.tournamentTitle}</div>
             <div className="tournament-detail">{tournament.author}</div>
             <div className="tournament-detail">{tournament.tournamentsDate}</div>
-            <div className="tournament-detail">{tournament.playersLimit}</div>
+            <div className="tournament-detail">{tournament.wasPlayedOut ? "finished" : "to be played"}</div>
         </div>
     );
 
+    const constructObject = () => {
+        const data = {
+            tournamentTitle,
+            minPlayOutDate,
+            maxPlayOutDate,
+            creator,
+            userParticipation
+        };
+
+        Object.keys(data).forEach(key => {
+            if (data[key] === undefined || data[key] === '') {
+                delete data[key];
+            }
+        });
+
+        return data;
+    };
+
+    const filterTournaments = async (e) => {
+        e.preventDefault();
+        const body = constructObject();
+        TournamentService.getFilteredTournaments(0, 10, body)
+        .then((response) => {
+            setFilteredTournaments(response.data.data.page);
+        }).catch((error) => {
+                setMessage('Error loading tournaments');
+        });
+    }
+    
     return (
         <div className="tournaments-container">
             <UserButtons/>
             <TournamentNav/>
             <div className="tournaments-box">
-                <h1>Upcoming Tournaments</h1>
-                {isAuthenticated && (
-                        <Link className="menu-btn" activeClassName="active" to="/tournaments/add">
-                            <button className="btn">Add Your Tournament</button>
-                        </Link>
-                    )}
-                <div className="tournament-headers">
-                    <span className="header-detail">Name</span>
-                    <span className="header-detail">Author</span>
-                    <span className="header-detail">Date</span>
-                    <span className="header-detail">Action</span>
+            {isAuthenticated && (
+            <Link className="menu-btn" activeClassName="active" to="/tournaments/add">
+                <button className="btn">Add Your Tournament</button>
+            </Link>
+            )}
+            <div className="tournaments-form-container">
+                <form onSubmit={filterTournaments}>
+                    <h2>Filter tournaments</h2>
+                    <div className="tournaments-form">
+                        <div>
+                        <label htmlFor="title">Title</label>
+                        <input
+                            type="text"
+                            id="title"
+                            onChange={(e) => setTournamentTitle(e.target.value)}
+                            placeholder="Enter tournament title"
+                            maxLength="30"
+                        /></div>
+                        <div><label htmlFor="minDate">From date</label>
+                        <input
+                            type="date"
+                            id="minDate"
+                            onChange={(e) => setMinPlayOutDate(e.target.value)}
+                            placeholder="Enter from date"
+                            maxLength="30"
+                        /></div>
+                        <div><label htmlFor="maxDate">To date</label>
+                        <input
+                            type="date"
+                            id="maxDate"
+                            onChange={(e) => setMaxPlayOutDate(e.target.value)}
+                            placeholder="Enter to date"
+                            maxLength="30"
+                        /></div>
+                        <div><label htmlFor="creator">Creator</label>
+                        <input
+                            type="text"
+                            id="creator"
+                            onChange={(e) => setCreator(e.target.value)}
+                            placeholder="Enter creator username"
+                            maxLength="30"
+                        /></div>
+                        <div><label htmlFor="partcipating">Partcipating user</label>
+                        <input
+                            type="text"
+                            id="partcipating"
+                            onChange={(e) => setUserParticipation(e.target.value)}
+                            placeholder="Enter username of partcipating user"
+                            maxLength="30"
+                        /></div>
+                        <button type="submit" className="submit">Filter</button>
+                        </div>
+                    </form>
                 </div>
-                <div className="tournaments-content">
+                {filteredTournaments.length < 1 &&  allTournaments.length > 0 ?
+                <div className="tournament-wrapper">
+                    <h1>Past Tournaments</h1>
+                    <div className="tournament-headers">
+                        <span className="header-detail">Name</span>
+                        <span className="header-detail">Author</span>
+                        <span className="header-detail">Date</span>
+                        <span className="header-detail">Status</span>
+                    </div>
+                    <div className="tournaments-content">
                     {isAuthenticated ?
-                    upcomingTournaments.map(tournament => (
+                    allTournaments.map(tournament => (
                         <div key={tournament.id}>
                             <TournamentItem key={tournament.id} tournament={tournament} />
                             <DeleteTournamentButton tournamentId={tournament.id} />
                         </div>
                     )) 
                     : 
-                    upcomingTournaments.map(tournament => (
+                    allTournaments.map(tournament => (
                         <TournamentItem key={tournament.id} tournament={tournament} />
                     ))}
                 </div>
-                <h1>Past Tournaments</h1>
-                <div className="tournament-headers">
-                    <span className="header-detail">Name</span>
-                    <span className="header-detail">Author</span>
-                    <span className="header-detail">Date</span>
-                    <span className="header-detail">Action</span>
                 </div>
-                <div className="tournaments-content">
-                    {pastTournaments.map(tournament => (
+                : filteredTournaments.length > 0 ? 
+                <div className="tournament-wrapper">
+                    <h1>Past Tournaments</h1>
+                    <div className="tournament-headers">
+                        <span className="header-detail">Name</span>
+                        <span className="header-detail">Author</span>
+                        <span className="header-detail">Date</span>
+                        <span className="header-detail">Status</span>
+                    </div>
+                    <div className="tournaments-content">
+                    {isAuthenticated ?
+                    filteredTournaments.map(tournament => (
+                        <div key={tournament.id}>
+                            <TournamentItem key={tournament.id} tournament={tournament} />
+                            <DeleteTournamentButton tournamentId={tournament.id} />
+                        </div>
+                    )) 
+                    : 
+                    filteredTournaments.map(tournament => (
                         <TournamentItem key={tournament.id} tournament={tournament} />
                     ))}
                 </div>
-            </div>
+                </div>
+                :<>There was an error fetching data</>}
+                </div>
             <p>{message}</p>
         </div>
     );
