805a7a137e3d0839213d3e1a5a800a844f113dbb
Mikołaj <mikibur@wp.pl>
2024-06-11 12:05:32 +0200

    Tournament details with tournament ladder visualisation

--- src/Tournaments/TournamentDetails.js
+++ src/Tournaments/TournamentDetails.js
@@ -7,6 +7,7 @@ import {login, logout} from '../User/store';
 import {TournamentService} from "../services/TournamentService";
 import React, {useEffect, useState} from "react";
 import {Buffer} from "buffer";
+import Ladder from "../elements/Ladder/Ladder";
 
 function TournamentDetails({isAuthenticated, user, login, logout}) {
 
@@ -14,6 +15,41 @@ function TournamentDetails({isAuthenticated, user, login, logout}) {
     const [tournament, setTournament] = useState([]);
     const [message, setMessage] = useState('');
 
+    const ladder = [
+        {"matchId":1,"PlayersBots":[{"BotId":8,"BotFile":"Back_jackbot_8","UserName":"emma_jenkins"},{"BotId":5,"BotFile":"Back_jackbot_5","UserName":"sam_wilson"}],"Status":"Resolve","Winner":5,"Position":4,"PlayedOutDate":"2024-06-10T09:32:46.4116135","MatchResult":"Won"},
+        {"matchId":2,"PlayersBots":[{"BotId":7,"BotFile":"Back_jackbot_7","UserName":"david_miller"},{"BotId":1,"BotFile":"Back_jackbot_1","UserName":"john_doe"}],"Status":"Resolve","Winner":7,"Position":3,"PlayedOutDate":"2024-06-10T09:32:47.7387294","MatchResult":"Won"},
+        {"matchId":3,"PlayersBots":[{"BotId":9,"BotFile":"Back_jackbot_9","UserName":"ryan_clark"},{"BotId":2,"BotFile":"Back_jackbot_2","UserName":"jane_smith"}],"Status":"Resolve","Winner":2,"Position":5,"PlayedOutDate":"2024-06-10T09:32:47.5846803","MatchResult":"Won"},
+        {"matchId":4,"PlayersBots":[{"BotId":6,"BotFile":"Back_jackbot_6","UserName":"olivia_brown"},{"BotId":2,"BotFile":"Back_jackbot_2","UserName":"jane_smith"}],"Status":"Resolve","Winner":2,"Position":2,"PlayedOutDate":"2024-06-10T09:32:56.4706626","MatchResult":"Won"},
+        {"matchId":5,"PlayersBots":[{"BotId":5,"BotFile":"Back_jackbot_5","UserName":"sam_wilson"},{"BotId":7,"BotFile":"Back_jackbot_7","UserName":"david_miller"}],"Status":"Resolve","Winner":7,"Position":1,"PlayedOutDate":"2024-06-10T09:32:55.2517037","MatchResult":"Won"},
+        {"matchId":6,"PlayersBots":[{"BotId":2,"BotFile":"Back_jackbot_2","UserName":"jane_smith"},{"BotId":7,"BotFile":"Back_jackbot_7","UserName":"david_miller"}],"Status":"Resolve","Winner":7,"Position":0,"PlayedOutDate":"2024-06-10T09:33:05.4557752","MatchResult":"Won"}
+    ]
+
+    useEffect(() => {
+        // Create a WebSocket instance
+        const socket = new WebSocket(`ws://10.242.93.198:8080/tournamentWs/${tournamentId}`);
+
+        // Event listeners
+        socket.addEventListener('open', () => {
+            console.log('Connected to WebSocket server');
+        });
+
+        socket.addEventListener('message', (event) => {
+            console.log('Received message from server:', event.data);
+            // setLadder(JSON.parse(event.data));
+            // Perform actions based on the received data
+        });
+
+        socket.addEventListener('close', () => {
+            console.log('Disconnected from WebSocket server');
+        });
+
+        // Clean up on unmount
+        return () => {
+            if (socket.readyState === 1) { // <-- This is important
+                socket.close();
+            }
+        }
+    }, []);
     useEffect(() => {
         TournamentService.getTournament(tournamentId)
             .then((response) => {
@@ -24,7 +60,6 @@ function TournamentDetails({isAuthenticated, user, login, logout}) {
         });
     }, []);
 
-    console.log(tournament);
     if (!tournament) {
         return <div>Tournament not found</div>;
     }
@@ -54,6 +89,9 @@ function TournamentDetails({isAuthenticated, user, login, logout}) {
             {isAuthenticated ? (
                 <button className="btn"><Link to={`/tournaments/edit/${tournament.id}`}>Edit Tournament</Link></button>
             ) : null}
+            {console.log(ladder)}
+        <Ladder status={ladder}/>
+
         </div>
     );
 }
